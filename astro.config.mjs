b573fe664b404472d8d28b3918844b3538abe77b
Tsatsu Amable <tsatsu.amable@gmail.com>
2026-09-06 22:52:20 +0100

    Fix GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://agalmicresearch.org',
+  site: 'https://tsatsuamable.github.io',
+  base: '/agalmicresearch',
 });
