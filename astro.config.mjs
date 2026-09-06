48fd30bc3fba9300ddfdd171bf737efa9c632682
Tsatsu Amable <tsatsu.amable@gmail.com>
2026-09-06 23:34:52 +0100

    Restore custom-domain Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://tsatsuamable.github.io',
-  base: '/agalmicresearch',
+  site: 'https://agalmicresearch.org',
 });
